9926f3cc2213a5db24dfd69253c9ebb385280a91
Cletus Samuel <109830247+CletsyMedia@users.noreply.github.com>
2023-01-23 06:34:45 +0100

    Summary

---
+++ Assets/AppJs/App.js
@@ -0,0 +1,62 @@
+let light = document.querySelector(".light--hidden");
+let dark =document.querySelector(".dark--hidden");
+
+light.onclick=()=>{
+    dark.classList.add("activeBtn");
+    document.body.classList.add("darkTheme")
+}
+dark.onclick=()=>{
+    dark.classList.remove("activeBtn")
+}
+
+const input = document.querySelector('#search');
+const searchBtn =  document.querySelector(".searchBtn");
+
+const user = document.querySelector(".githubUser");
+const login = document.querySelector(".githubUserName");
+const joined = document.querySelector(".githubJoinedDate");
+const repo = document.querySelector(".repoTotal");
+const follower = document.querySelector(".followerTotal");
+const followings = document.querySelector(".followingTotal");
+const locations = document.querySelector(".locations");
+const twit = document.querySelector(".twit");
+const websites = document.querySelector(".web");
+const companies = document.querySelector(".companies");
+const githubBio = document.querySelector(".githubBio");
+let img = document.createElement("img");
+let block = document.querySelector(".mainImg");
+// const url = `https://api.github.com/users/CletsyMedia${input.value}`;
+
+
+
+
+searchBtn.addEventListener("click", function(){
+    const url = `https://api.github.com/users/${input.value}`;
+    async function getUrl(){
+        const response = await fetch(url);
+        const data = await response.json();
+        const dataDate = data.created_at.slice(0, data.created_at.length - 10);
+
+        img.src = data.avatar_url;
+        block.appendChild(img);
+        block.style.border="none";
+        user.innerText = data.name;
+        login.innerHTML = `@${data.login}`
+        joined.innerText = `Joined ${dataDate}`
+        repo.innerText = data.public_repos;
+        follower.innerText = data.followers;
+        followings.innerText = data.following;
+        locations.innerText = data.location === "" || data.location === null 
+            ? "No location" : data.location
+        twit.innerText = data.twitter_username === "" || data.twitter_username === null 
+            ? "Not a user" : data.twitter_username;
+        websites.innerText = data.blog === "" || data.blog === null 
+            ? "No website" : data.blog;
+        companies.innerText = data.company === "" || data.company === null 
+            ? "No company" : data.company;
+        githubBio.innerText = data.bio === "" || data.bio === null 
+            ? "This profile has no bio...." : data.bio;
+    }
+    getUrl();
+    input.value="";
+})
